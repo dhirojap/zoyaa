0d021d1df2ac8a0c06d3f8a2294ff70951fdbb80
DhiroJap <dhiro.jap@binus.ac.id>
2023-12-25 19:16:52 +0700

    Initial commit

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: 'Free',
+    slug: 'free',
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: '',
+        production: '',
+      },
+    },
+  },
+  {
+    name: 'Zee Nation',
+    slug: 'zee nation',
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      amount: 50000,
+      priceIds: {
+        test: 'price_1ORBrbFEG4u1jUfahg3lhVnO',
+        production: '',
+      },
+    },
+  },
+];
